3b9c5e6cbba440c68cac273efae99d94514d1201
Prof Edir <ecaramba@gmail.com>
2025-02-26 19:56:36 -0300

    aula7

---
+++ aula7.js
@@ -0,0 +1,47 @@
+
+var cidade = "Curitiba";
+var cidade2 = "Pinhais";
+
+// lista -> vetores -> array
+var cidades = ["Curitiba", "Pinhais", "Araucaria"];
+
+// add um item no vetor
+cidades.push("Colombo");
+
+// remove curitiba
+// cidades.shift();
+
+// apaga pinhais
+//cidades.splice(1, 1)
+
+cidades[1] = "Piraquara";
+
+console.log(cidades);
+
+
+// loops -> repetição
+
+var ini = 0;
+var fim = cidades.length - 1;
+
+while (ini <= fim) 
+{
+    console.log(cidades[ini]);
+
+    ini = ini + 1;
+} 
+
+var n1 = 2;
+var n2 = 3;
+var n3 = 8;
+
+function soma(n1, n2)
+{
+    var resultado = n1 + n2 + n3;
+    return resultado;
+}
+
+
+var val = soma(3,5);
+
+console.log(val);
