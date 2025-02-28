cb8b94ec7f8cb4195be23ed555ad35fd0df96088
Prof Edir <ecaramba@gmail.com>
2025-02-27 21:46:25 -0300

    jquery

--- jquery.js
+++ jquery.js
@@ -8,5 +8,33 @@ function mudar()
 function add()
 {
     var cidade = $("#cidade").val();
-    $("#lista").append("<li>" + cidade + "</li>");
+    var primeira = cidade.charAt(0);
+    var resto = cidade.substring(1);
+
+    var nova = primeira.toUpperCase() + resto.toLowerCase();
+    
+    
+    if ($("#lista").html().includes(nova) == true )
+    {
+        alert("cidade já cadastrada");
+    } else 
+    {
+
+        $("#lista").append("<li>" + nova + "</li>");
+    }
+    
 }
+
+// documento carregado
+$(document).ready(function(){
+
+    // add um evento
+    $(".bt-add").click(add);
+    
+    $(".titulo").css("color", "red");
+
+    $("h1, li").click(function(){
+        var nova = $("#cor").val();
+        $(this).css("color", nova)
+    });
+});
