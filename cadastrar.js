8abb1dfe14a9ff02bb5124d41435347c6c04a0df
Prof Edir <ecaramba@gmail.com>
2025-03-07 20:25:40 -0300

    fim do cadastro

--- cadastrar.js
+++ cadastrar.js
@@ -1,10 +1,54 @@
 $(document).ready(function(){
 
+    var dados = ["Pedro", "Ana", "Maria"];
+
+
+    function imprimir()
+    {
+        $("#lista").empty(); // limpa a lista
+
+        dados.forEach(function(item, idx){
+            $("#lista").append('<li class="list-group-item d-flex justify-content-between">'
+                + item +
+                '<button idx="'+ idx +'" class="btn btn-danger bt-del">Deletar</button></li>');
+        }); // fim do foreach
+    }
+
+    imprimir();
+
+
     $(".bt-add").click(function()
     {
         var nome = $("#nome").val();
 
-        $("#lista").append('<li class="list-group-item">'+ nome +'</li>');
+        if (dados.includes(nome) == true)
+        {
+            alert("nome já cadastrado")
+        } else{
+
+            dados.push(nome);
+            
+            imprimir();
+        }
+        
     }); // fim bt-add
+
+    $(".bt-ord").click(function(){
+        dados.sort();
+
+        imprimir();
+
+    }); //fim do bt-ord
+
+    // monitorando #lista
+    $("#lista").on("click", ".bt-del", function(){
+
+        var idx = $(this).attr("idx");
+        console.log(idx);
+        dados.splice(idx, 1);
+
+        imprimir();
+
+    }); //fim do bt-del
 
 });
